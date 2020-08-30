c78725e6917225f74391d0eb9a3fe58dceef0d7f
Zdenko Vrabel <zdenko.vrabel@unravela.io>
2020-08-30 23:42:29 +0200

    feat: version
    The version information is now available via -v option.
    
    spent 5m

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"os"
 )
 
+// version is set by goreleaser, via -ldflags="-X 'main.version=...'".
+var version = "development"
+
 func main() {
 	app := &cli.App{
 		Name:  "artisan",
+		Version: version,
 		Usage: "Artisan build orchestrator tool",
 		Commands: []*cli.Command{
 			run.RunCmd,
